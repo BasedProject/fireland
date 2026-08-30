298c5f29aef55a0bbd711f1d35d4dd5e83dc48a8
Emil Williams <emil@chud.cyou>
2026-08-30 13:11:25 +0000

    player: basic implemented

--- source/main.cpp
+++ source/main.cpp
@@ -19,7 +19,6 @@ using namespace std;
 
 #include "raylib-extra.hpp"
 
-v2 screen_area[1];
 
 #include "meat.hpp"
 
@@ -35,38 +34,39 @@ int TICK = 0;
 #include "player.hpp"
 
 int main(int ac, char ** av)
-{   char * program_name = av[0];
+{   (void)ac;
+    char * program_name = av[0];
     rl_screen screen[1];
+    v2 screen_area[1];
     v2 physical_area[1];
+    v2 mapshape[1] = {v2{12,8}};
+    player_t player[1] = {};
+    player->position[0] = {100, 100, 0};
+    Board board = Board(mapshape->x, mapshape->y);
     // Texture texture[TEXTURE_END];
     // Sound sound[SOUND_END];
 
     // change_directory(program_name);
 
+    screen_area[0] = get_board_display_size(board);
     meat_init(program_name, screen, screen_area, physical_area);
 
-    v2 mapshape[1] = {12,8};
-
-    player_t player[0] = {};
-    Board board = Board(mapshape->x, mapshape->y); // AM: thoughts on v2 being split on input in C++ (primarily C++ at all)
     randomize_board(board);
     random_fires(board, 3);
-
-    v2 screen_space_used_by_board = get_board_display_size(board);
 
     while (!WindowShouldClose()) {
         // Update
         update_fire_spread(board);
         physical_area[0] = rl_get_render_area();
-        update_player(player, rl_screen_shape(screen));
+        update_player(player, rl_v2_rectangle_wh(screen_area[0]));
         ++TICK;
 
         {   BeginTextureMode(screen[0]);
             ClearBackground(GREEN);
             draw_board(board);
-            draw_player(player, 50, rl_screen_shape(screen));
             draw_debug_fire(board);
             draw_debug_grid();
+            draw_player(player, 50, rl_v2_rectangle_wh(screen_area[0]));
             EndTextureMode();
         }
 
@@ -76,12 +76,12 @@ int main(int ac, char ** av)
                 screen[0].texture,
                 (Rectangle){
                     0,
-                    -screen_space_used_by_board.y,
-                    screen_space_used_by_board.x,
-                    -screen_space_used_by_board.y
+                    -screen_area->y,
+                    screen_area->x,
+                    -screen_area->y
                 },
                 rl_v4_rectangle(
-                    rl_fit_centered(screen_space_used_by_board, screen_area[0])
+                    rl_fit_centered(screen_area[0], physical_area[0])
                 ),
                 (v2){ 0, 0 },
                 0,
